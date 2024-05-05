a92726b3038310ff8895d0158dc4dd0d9b80685e
apaul42 <avishekpaul814@gmail.com>
2024-05-05 15:15:34 +0530

    deployments changes 2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const myServer = require('socket.io')(process.env.port || 3000, {
     cors: {
-        origin: ['http://localhost:8080']
+        origin: ['http://localhost:8080','https://tic-tac-toe-multiplayer1439.netlify.app/']
     }
 })
 
